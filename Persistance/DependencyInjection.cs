6d4eaa65ce44abbb6a0a437b12a143e76595f63a
renrenaa <nikita-gusev.2001@mail.ru>
2022-08-22 19:49:55 +0500

    Добавил регистрацию групп сервисов(Application и Persistance)

---
+++ Persistance/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Drugstore.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<DrugstoreDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IDrugstoreDbContext>(provider =>
+                provider.GetService<DrugstoreDbContext>());
+
+            return services;
+        }
+    }
+}
